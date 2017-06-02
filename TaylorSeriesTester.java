8028c5d3b3ef838385b337ea2518e12cf6466854
Jedimaster4559 <nbsolomon4559@gmail.com>
2017-06-02 14:29:46 -0500

    Modified structure to improve organization. Added printResults() method to provide summary of all tests performed

--- TaylorSeriesTester.java
+++ TaylorSeriesTester.java
@@ -1,11 +1,51 @@
-
+import java.util.List;
+import java.util.ArrayList;
 public class TaylorSeriesTester {
+	
+	private static List<String> passed = new ArrayList<String>();
+	private static List<String> failed = new ArrayList<String>();
 
 	public static void main(String[] args) {
-		TaylorSeries taylor = new TaylorSeries();
-		taylor.geometricSeriesApproximation(.5);
-		System.out.println(taylor.getApproximation());
+		geometricTest();
+		System.out.println();
+		
+		printResults();
+		
+		
 
+	}
+	
+	public static void geometricTest(){
+		TaylorSeries taylor = new TaylorSeries();
+		taylor.geometricSeriesApproximation(.5);			//tests .5 to determine if approximation is accurate
+		if(taylor.getApproximation() != 1.99993896484375){	//result of approximation
+			System.out.println("Geometric Failed");
+			failed.add("Geometric Failed");
+		}
+		else{
+			System.out.println("Geometric Passed");
+			passed.add("Geometric Passed");
+		}
+	}
+	
+	public static void printResults(){
+		System.out.println("Tests that Passed");
+		System.out.println("-----------------");
+		for(String x:passed){
+			System.out.println(x);
+		}
+		System.out.println("-----------------");
+		System.out.println("Total Passed: " + passed.size());
+		System.out.println();
+		
+		System.out.println("Tests that Failed");
+		System.out.println("-----------------");
+		for(String x:failed){
+			System.out.println(x);
+		}
+		System.out.println("-----------------");
+		System.out.println("Total Failed: " + failed.size());
+		
 	}
 
 }
